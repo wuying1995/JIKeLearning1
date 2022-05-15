1a401856b83dbff9020aba9854107f66f6cee5d7
W <1042364042@qq.com>
2022-05-15 23:32:26 +0800

    栈转队列

---
+++ src/main/java/com/example/demo/code/letcode09.java
@@ -0,0 +1,33 @@
+package com.example.demo.code;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+//两个栈实现队列
+public class letcode09 {
+        Stack<Integer> stack1;
+        Stack<Integer> stack2 ;
+
+        public letcode09() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+        }
+
+        public void appendTail(int value) {
+            stack1.push(value);
+
+        }
+
+        public int deleteHead() {
+            if(!stack2.isEmpty()){
+                return stack2.pop();
+            }else{
+                while(!stack1.isEmpty()){
+                    stack2.push(stack1.pop());
+                }
+                return stack2.isEmpty()?-1:stack2.pop();
+            }
+        }
+    }
